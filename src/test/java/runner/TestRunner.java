863c1e9074ce1a2f46ecdc954afffa6d1f0c9257
Mithun <asdmithun@gmail.com>
2021-09-27 13:37:33 -0400

    13th commit

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features = "src/test/resources/Features",
         glue = {"step_definitions"},
-        plugin = {"pretty", "summary"},
+        plugin = {"pretty", "summary","html:target/CucumberReports/reports.html"},
         publish = true,
         dryRun = false
 //        tags="@SmokeTesting"
