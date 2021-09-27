60ad516944155b5022848a9f1f2789e53ca039d7
Mithun <asdmithun@gmail.com>
2021-09-27 14:39:45 -0400

    15th commit

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -8,10 +8,10 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features = "src/test/resources/Features",
         glue = {"step_definitions"},
-        plugin = {"pretty", "summary","html:target/CucumberReports/reports.html"},
-        publish = true,
-        dryRun = false
-//        tags="@SmokeTesting"
+        plugin = {"pretty", "summary","json:target/cucumber.json"},
+//        publish = true,
+//        dryRun = false,
+        tags="@SmokeTesting"
 )
 public class TestRunner {
 }
